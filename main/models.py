adf66087b1a64a5d1dc98b9993aebfa2806990a0
Naurah15 <naunaurahiradya@gmail.com>
2024-09-17 18:46:58 +0700

    tugas 3

--- main/models.py
+++ main/models.py
@@ -1,16 +1,17 @@
+import uuid
 from django.db import models
 
 class Item(models.Model):
     # Atribut wajib
-    name = models.CharField(max_length=255)
-    price = models.IntegerField()
-    description = models.TextField()
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False) # untuk menambahkan id pengguna
+    name = models.CharField(max_length=255) # untuk memasukkan nama pengguna
+    price = models.IntegerField() # untuk memasukkan harga makanan
+    description = models.TextField() # untuk memasukkan deskripsi dari makanan sesuai keinginan customer
 
     # Atribut tambahan yang relevan untuk e-commerce
     stock = models.IntegerField(default=0)  # Stok barang
     category = models.CharField(max_length=100)  # Kategori barang
     rating = models.DecimalField(max_digits=3, decimal_places=2, default=0.00)  # Rating barang
-    date_added = models.DateField(auto_now_add=True)  # Tanggal barang ditambahkan
     discount = models.DecimalField(max_digits=5, decimal_places=2, default=0.00)  # Diskon barang dalam persentase
 
     def __str__(self):
--- main/views.py
+++ main/views.py
@@ -1,11 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect   # Tambahkan import redirect di baris ini
+from main.forms import ItemForm
+from main.models import Item
+from django.http import HttpResponse
+from django.core import serializers
 
 # Create your views here.
 def show_main(request):
+    item_entries = Item.objects.all()
     context = {
         'npm' : '2306245900',
         'name': 'Naurah Iradya Kurniawan',
-        'class': 'PBP B'
+        'class': 'PBP B',
+        'item_entries': item_entries
+        
     }
 
     return render(request, "main.html", context)
+
+def create_item(request):
+    form = ItemForm(request.POST or None)
+
+    if form.is_valid() and request.method == "POST":
+        form.save()
+        return redirect('main:show_main')
+
+    context = {'form': form}
+    return render(request, "create_item.html", context)
+
+def show_xml(request):
+    data = Item.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json(request):
+    data = Item.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_xml_by_id(request, id):
+    data = Item.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json_by_id(request, id):
+    data = Item.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
